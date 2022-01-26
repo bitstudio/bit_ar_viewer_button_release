5cb4b05456fdee01fb89b512a3ae9d7e9e52af72
nattadetspite <ntd.ctnts@gmail.com>
2022-01-26 16:46:41 +0700

    button using text and logo not img

--- ar-button.js
+++ ar-button.js
@@ -6,29 +6,27 @@ function isIOS() {
 }
 
 const qrcode_url = "https://bit-ar-viewer-qrcode.web.app/?endpoint=";
+const model_fetcher_url = "https://model-fetcher-website-a4ghjocxma-uc.a.run.app";
 const url_endpoint = "https://arhub.app/";
 
 function init() {
-  // function loadFont() {
-  //   const webfontScript = document.createElement("script");
-  //   webfontScript.type = "text/javascript";
-  //   webfontScript.src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
+  function loadFont() {
+    const webfontScript = document.createElement("script");
+    webfontScript.type = "text/javascript";
+    webfontScript.src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
 
-  //   webfontScript.onload = function () {
-  //     WebFont.load({
-  //       google: {
-  //         families: ["Kanit"],
-  //       },
-  //     });
-  //   };
-
-  //   document.head.appendChild(webfontScript);
-  // }
-
+    webfontScript.onload = function () {
+      WebFont.load({
+        google: {
+          families: ["Kanit"],
+        },
+      });
+    };
+    document.head.appendChild(webfontScript);
+  }
   const makeARbutton = async () => {
     const deviceType = isAndroid() ? "android" : isIOS() ? "ios" : "desktop";
-    // loadFont();
-    // enter partner ID here
+    loadFont();
     let partnerID = undefined;
     try {
       partnerID = window.partnerID;
@@ -39,18 +37,50 @@ function init() {
     const buttons = document.getElementsByClassName("ar-hub-btn");
 
     for (const button of buttons) {
-      let nodeList = button.childNodes;
-      for (const nodeItem of nodeList) {
-        if (nodeItem.nodeName === "IMG") {
-          nodeItem.src =
-            "https://storage.googleapis.com/bit-ar-viewer/commer_button/ar_icon_w3.png";
-          nodeItem.style.height = "2rem";
-        }
-      }
+      let wrapper = document.createElement("div");
+      let text_a = document.createElement("p");
+      text_a.style.margin = "0";
+      text_a.innerText = "ดูโมเดลด้วย";
+      let icon = document.createElement("img");
+      icon.style.height = "2rem";
+      icon.style.width = "2rem";
+      icon.style.margin = "0 .2rem";
+      icon.src = "https://storage.googleapis.com/bit-ar-viewer/commer_button/ar_icon.svg";
+      let text_b = document.createElement("p");
+      text_b.style.margin = "0";
+      text_b.innerText = "ARhub";
+      wrapper.appendChild(text_a);
+      wrapper.appendChild(icon);
+      wrapper.appendChild(text_b);
+
+      // wrapper.style = {
+      //   height: "100%",
+      //   width: "100%",
+      //   display: "flex",
+      //   justifyContent: "row",
+      // };
+
+      wrapper.style.height = "100%";
+      wrapper.style.width = "100%";
+      wrapper.style.display = "flex";
+      wrapper.style.justifyContent = "center";
+      wrapper.style.alignItems = "center";
+
+      button.appendChild(wrapper);
+
+      // let nodeList = button.childNodes;
+      // for (const nodeItem of nodeList) {
+      //   if (nodeItem.nodeName === "IMG") {
+      //     nodeItem.src = "https://storage.googleapis.com/bit-ar-viewer/commer_button/ar_icon.svg";
+      //     nodeItem.style.height = "2rem";
+      //   }
+      // }
 
       button.style.display = "flex";
       button.style.width = "100%";
       button.style.maxWidth = "350px";
+      button.style.height = "100%";
+      button.style.maxHeight = "250px";
       button.style.paddingTop = "1rem";
       button.style.paddingBottom = "1rem";
       button.style.paddingLeft = "2rem";
@@ -65,13 +95,15 @@ function init() {
       button.style.fontStyle = "Regular";
       button.style.fontSize = "1.6rem";
       button.style.lineHeight = "140%";
+      button.style.margin = "auto";
+      button.style.border = "none";
 
       button.style.cursor = "pointer";
       button.style.textDecoration = "none";
 
-      const img = document.getElementsByClassName("ar-hub-btn")[0].children[0];
-      img.style.width = "100%";
-      img.style.height = "auto";
+      // const img = document.getElementsByClassName("ar-hub-btn")[0].children[0];
+      // img.style.width = "100%";
+      // img.style.height = "auto";
 
       const productID = button.getAttribute("data-product-id");
 
@@ -82,68 +114,8 @@ function init() {
           window.open(qrcode_url + url_endpoint + "products/" + productID, "_blank");
           return;
         } else {
-          const res = await fetch(
-            "https://us-central1-bit-ar-viewer.cloudfunctions.net/button_ar_viewer_api/button_api",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                partnerID: partnerID,
-                productID: productID,
-                deviceType: deviceType,
-              }),
-            }
-          );
-          const ar_model_url = await res.json();
-          switch (deviceType) {
-            case "ios":
-              button.href = ar_model_url + "#allowsContentScaling=0";
-              button.click();
-              button.addEventListener("click", handleClickButton);
-              break;
-            case "android":
-              button.addEventListener("click", handleClickButton);
-              const fallbackUrl = "https://bit.studio";
-              const sceneViewerOptions = {
-                mode: "ar_only", // ar_preferred
-                scheme: "https",
-                package: "com.google.android.googlequicksearchbox",
-                action: "android.intent.action.VIEW",
-                fallback: fallbackUrl,
-                link: "https://bit.studio/ARhub",
-                title: "Learn more about our AR service",
-                resizable: "false",
-              };
-              const encodedURL = encodeURIComponent(ar_model_url);
-
-              const sceneViewerUrl =
-                "intent://arvr.google.com/scene-viewer/1.0?file=" +
-                encodedURL +
-                "&mode=" +
-                sceneViewerOptions.mode +
-                "&link=" +
-                sceneViewerOptions.link +
-                "&title=" +
-                sceneViewerOptions.title +
-                "&resizable=" +
-                sceneViewerOptions.resizable +
-                "#Intent;scheme=" +
-                sceneViewerOptions.scheme +
-                ";package=" +
-                sceneViewerOptions.package +
-                ";action=" +
-                sceneViewerOptions.action +
-                ";S.browser_fallback_url=" +
-                fallbackUrl +
-                ";end;";
-              window.open(sceneViewerUrl);
-              break;
-            default:
-              button.addEventListener("click", handleClickButton);
-              break;
-          }
+          button.addEventListener("click", handleClickButton);
+          window.open(model_fetcher_url + "/?id=" + productID);
         }
       };
       button.addEventListener("click", handleClickButton);
@@ -154,5 +126,6 @@ function init() {
 }
 
 window.addEventListener("load", () => {
+  console.log("load js");
   init();
 });
