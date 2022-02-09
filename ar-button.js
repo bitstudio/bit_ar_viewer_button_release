72e0ff752f9615f9a6e1e859c91c11d246ea4ab1
bit-spite <spite@thebitstudio.com>
2022-02-09 15:55:29 +0700

    clean unused code

--- ar-button.js
+++ ar-button.js
@@ -32,55 +32,15 @@ function init() {
       let wrapper = document.createElement("div");
       wrapper.classList.add("content-wrapper");
       let text_a = document.createElement("p");
-      // text_a.style.margin = "0";
       text_a.innerText = "ดูโมเดลด้วย";
-      // text_a.style.whiteSpace = "pre";
-      // text_a.style.fontSize = "2.8rem";
       let icon = document.createElement("img");
-      // icon.style.height = "60%";
-      // icon.style.width = "auto";
-      // icon.style.margin = "0 0.6rem";
       icon.src = "https://storage.googleapis.com/bit-ar-viewer/commer_button/ar_icon.svg";
       let text_b = document.createElement("p");
-      // text_b.style.margin = "0";
       text_b.innerText = "ARhub";
-      // text_b.style.fontSize = "2.8rem";
       wrapper.appendChild(text_a);
       wrapper.appendChild(icon);
       wrapper.appendChild(text_b);
-
-      // wrapper.style.height = "8rem";
-      // wrapper.style.width = "100%";
-      // wrapper.style.display = "flex";
-      // wrapper.style.justifyContent = "center";
-      // wrapper.style.alignItems = "center";
-
       button.appendChild(wrapper);
-
-      // button.style.display = "flex";
-      // button.style.width = "100%";
-      // button.style.maxWidth = "360px";
-      // button.style.height = "100%";
-      // button.style.maxHeight = "250px";
-      // button.style.paddingTop = "1rem";
-      // button.style.paddingBottom = "1rem";
-      // button.style.paddingLeft = "2rem";
-      // button.style.paddingRight = "2rem";
-      // button.style.background = "#0025f5";
-      // button.style.color = "white";
-      // button.style.borderRadius = "45px";
-      // button.style.textAlign = "center";
-      // button.style.justifyContent = "center";
-      // button.style.alignItems = "center";
-      // button.style.fontFamily = "Kanit";
-      // button.style.fontStyle = "Regular";
-      // button.style.fontSize = "1.6rem";
-      // button.style.lineHeight = "140%";
-      // button.style.margin = "auto";
-      // button.style.border = "none";
-
-      // button.style.cursor = "pointer";
-      // button.style.textDecoration = "none";
 
       const productID = button.getAttribute("data-product-id");
 
