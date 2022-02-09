bbc0c03ec094d5970842aaba9549c92d6ac20277
nattadetspite <ntd.ctnts@gmail.com>
2022-02-09 15:00:29 +0700

    disable check desktop, styles in styles.css

--- ar-button.js
+++ ar-button.js
@@ -1,11 +1,3 @@
-function isAndroid() {
-  return /(android)/i.test(window.navigator.userAgent);
-}
-function isIOS() {
-  return !!window.navigator.userAgent && /iPad|iPhone|iPod/.test(window.navigator.userAgent);
-}
-
-const qrcode_url = "https://bit-ar-viewer-qrcode.web.app/?endpoint=";
 const model_fetcher_url = "https://model-fetcher-website-a4ghjocxma-uc.a.run.app";
 const url_endpoint = "https://arhub.app/";
 
@@ -25,8 +17,8 @@ function init() {
     document.head.appendChild(webfontScript);
   }
   const makeARbutton = async () => {
-    const deviceType = isAndroid() ? "android" : isIOS() ? "ios" : "desktop";
     loadFont();
+
     let partnerID = undefined;
     try {
       partnerID = window.partnerID;
@@ -39,87 +31,62 @@ function init() {
     for (const button of buttons) {
       let wrapper = document.createElement("div");
       let text_a = document.createElement("p");
-      text_a.style.margin = "0";
+      // text_a.style.margin = "0";
       text_a.innerText = "ดูโมเดลด้วย";
-      text_a.style.whiteSpace = "pre";
-      text_a.style.fontSize = "2.8rem";
+      // text_a.style.whiteSpace = "pre";
+      // text_a.style.fontSize = "2.8rem";
       let icon = document.createElement("img");
-      icon.style.height = "60%";
-      icon.style.width = "auto";
-      icon.style.margin = "0 0.6rem";
+      // icon.style.height = "60%";
+      // icon.style.width = "auto";
+      // icon.style.margin = "0 0.6rem";
       icon.src = "https://storage.googleapis.com/bit-ar-viewer/commer_button/ar_icon.svg";
       let text_b = document.createElement("p");
-      text_b.style.margin = "0";
+      // text_b.style.margin = "0";
       text_b.innerText = "ARhub";
-      text_b.style.fontSize = "2.8rem";
+      // text_b.style.fontSize = "2.8rem";
       wrapper.appendChild(text_a);
       wrapper.appendChild(icon);
       wrapper.appendChild(text_b);
 
-      // wrapper.style = {
-      //   height: "100%",
-      //   width: "100%",
-      //   display: "flex",
-      //   justifyContent: "row",
-      // };
-
-      wrapper.style.height = "8rem";
-      wrapper.style.width = "100%";
-      wrapper.style.display = "flex";
-      wrapper.style.justifyContent = "center";
-      wrapper.style.alignItems = "center";
+      // wrapper.style.height = "8rem";
+      // wrapper.style.width = "100%";
+      // wrapper.style.display = "flex";
+      // wrapper.style.justifyContent = "center";
+      // wrapper.style.alignItems = "center";
 
       button.appendChild(wrapper);
 
-      // let nodeList = button.childNodes;
-      // for (const nodeItem of nodeList) {
-      //   if (nodeItem.nodeName === "IMG") {
-      //     nodeItem.src = "https://storage.googleapis.com/bit-ar-viewer/commer_button/ar_icon.svg";
-      //     nodeItem.style.height = "2rem";
-      //   }
-      // }
+      // button.style.display = "flex";
+      // button.style.width = "100%";
+      // button.style.maxWidth = "360px";
+      // button.style.height = "100%";
+      // button.style.maxHeight = "250px";
+      // button.style.paddingTop = "1rem";
+      // button.style.paddingBottom = "1rem";
+      // button.style.paddingLeft = "2rem";
+      // button.style.paddingRight = "2rem";
+      // button.style.background = "#0025f5";
+      // button.style.color = "white";
+      // button.style.borderRadius = "45px";
+      // button.style.textAlign = "center";
+      // button.style.justifyContent = "center";
+      // button.style.alignItems = "center";
+      // button.style.fontFamily = "Kanit";
+      // button.style.fontStyle = "Regular";
+      // button.style.fontSize = "1.6rem";
+      // button.style.lineHeight = "140%";
+      // button.style.margin = "auto";
+      // button.style.border = "none";
 
-      button.style.display = "flex";
-      button.style.width = "100%";
-      button.style.maxWidth = "360px";
-      button.style.height = "100%";
-      button.style.maxHeight = "250px";
-      button.style.paddingTop = "1rem";
-      button.style.paddingBottom = "1rem";
-      button.style.paddingLeft = "2rem";
-      button.style.paddingRight = "2rem";
-      button.style.background = "#0025f5";
-      button.style.color = "white";
-      button.style.borderRadius = "45px";
-      button.style.textAlign = "center";
-      button.style.justifyContent = "center";
-      button.style.alignItems = "center";
-      button.style.fontFamily = "Kanit";
-      button.style.fontStyle = "Regular";
-      button.style.fontSize = "1.6rem";
-      button.style.lineHeight = "140%";
-      button.style.margin = "auto";
-      button.style.border = "none";
-
-      button.style.cursor = "pointer";
-      button.style.textDecoration = "none";
-
-      // const img = document.getElementsByClassName("ar-hub-btn")[0].children[0];
-      // img.style.width = "100%";
-      // img.style.height = "auto";
+      // button.style.cursor = "pointer";
+      // button.style.textDecoration = "none";
 
       const productID = button.getAttribute("data-product-id");
 
       const handleClickButton = async () => {
         button.removeEventListener("click", handleClickButton);
-        if (deviceType === "desktop") {
-          button.addEventListener("click", handleClickButton);
-          window.open(qrcode_url + model_fetcher_url + "/?id=" + productID, "_blank");
-          return;
-        } else {
-          button.addEventListener("click", handleClickButton);
-          window.open(model_fetcher_url + "/?id=" + productID);
-        }
+        button.addEventListener("click", handleClickButton);
+        window.open(model_fetcher_url + "/?id=" + productID);
       };
       button.addEventListener("click", handleClickButton);
     }
